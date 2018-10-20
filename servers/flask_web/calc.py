96c84b87969c13c62d012c4b37906d76a7f88350
umetsuyoshinori <umetsu.yoshinori.uv4@is.naist.jp>
2018-10-20 23:25:05 +0900

    Fix minimize whole system

--- servers/flask_web/calc.py
+++ servers/flask_web/calc.py
@@ -1,3 +1,36 @@
 def calc(wetness, temperature, humidity):
     rest_of_time = 1.0
+
+    
+
+#
+#
+# def example_digits(num):
+#
+#     #scilit-learnライブラリに付属しているデータセット"digits"を利用
+#     digits = datasets.load_digits()
+#
+#     #サポートベクターマシン（というもの）を使って、分類.
+#     # 64個のint配列:正解の数字
+#     clf = svm.SVC()
+#     clf.fit(digits.data,digits.target)
+#
+#     #仮に下記の"test_data"のようなデータは、どの数字に該当するか？を予測
+#     test = [num]*64
+#     test_data = [test]
+#
+#     c1 = "-----テスト----"
+#     c2 = str(test_data)
+#
+#     d1 = "-----予測----"
+#     global pre
+#     pre = clf.predict(test_data)
+#     d2 = str(pre)
+#
+#     text = c1+"<br>"+c2+"<br>"+d1+"<br>"+d2
+#
+#     return(text)
+#
+#
+
     return rest_of_time
