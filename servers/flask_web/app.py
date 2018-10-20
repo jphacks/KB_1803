4f07beb8649ff7143c7735723ec0d903ff1a1dfa
Atsushi Otsubo <m@rirex.net>
2018-10-20 17:47:05 +0900

    Make api server

--- servers/flask_web/app.py
+++ servers/flask_web/app.py
@@ -5,54 +5,64 @@ from influxdb import InfluxDBClient
 from pytz import timezone
 from datetime import datetime
 
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, make_response
 import json
 app = Flask(__name__)
 
+@app.route("/api", methods=['GET'])
+def sensor_request_api():
+    params = request.args
+    wetness = params.get('wetness', default='0', type = str)
+    temperature = params.get('temperature', default='0', type = str)
+    humidity = params.get('humidity', default='0', type = str)
+    co2 = params.get('co2', default='0', type = str)
+    tvoc = params.get('tvoc', default='0', type = str)
+
+    write(wetness, temperature, humidity, co2, tvoc)
+    return 'hoge'###"wetness" + wetness + " temperature:" + temperature + " humidity" + humidity + " co2" + co2 + " tvoc:" + tvoc
+
 @app.route("/", methods=['GET'])
-def hello():
+def index():
 
     client = InfluxDBClient(host='influxdb', port=8086, database='superdry')
     prediction = client.query("select * from prediction where time >= now() - 300m")
-
 
     # sensordata = client.query("select * from sensordata")
     # print("prediction: {0}".format(prediction))
     # return "prediction: {0}".format(prediction)
     return render_template('index.html')
 
-@app.route('/write', methods=['GET'])
-def write():
+# @app.route('/write', methods=['GET'])
+def write(wetness, temperature, humidity, co2, tvoc):
 
-# 現在時刻
     utc_now = datetime.now(timezone('UTC'))
     jst_now = utc_now.astimezone(timezone('Asia/Tokyo'))
-    def write(host='influxdb',port=8086,per=None,time=None):
-        # """Instantiate a connection to the InfluxDB."""
-        user = 'root'
-        password = 'root'
-        dbname = 'superdry'
-        json_body = [
-            {
-                "measurement": "prediction",
-                "tags": {
-                    "user": "umetsu",
-                    "id": "0"},
-                "time": str(jst_now),
-                "fields": {
-                    "dryness": per,
-                    "rest_of_time": time,
-                }
+    host='influxdb'
+    port=8086
+    user = 'root'
+    password = 'root'
+    dbname = 'superdry'
+    json_body = [
+        {
+            "measurement": "sensordata",
+            "tags": {
+                "user": "umetsu",
+                "id": "0"},
+            "time": str(jst_now),
+            "fields": {
+                "wetness": float(wetness),
+                "temperature": float(temperature),
+                "humidity": float(humidity),
+                "co2": float(co2),
+                "tvoc": float(tvoc)
             }
-        ]
+        }
+    ]
 
-        print("client: ",host, port, user, password, dbname)
-        client = InfluxDBClient(host, port, user, password, dbname)
-        client.create_database(dbname)
-        client.write_points(json_body)
-
-    write(per=0.5, time=30)
-
+    print("client: ",host, port, user, password, dbname)
+    client = InfluxDBClient(host, port, user, password, dbname)
+    client.create_database(dbname)
+    client.write_points(json_body)
 
     return "write"
     # answer = "Yes, it is %s!\n" % data["keyword"]
