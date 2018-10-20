cf7e7f3a66e54b14b2bfda3aca80ad5cfc5bc1a3
Gasuta722 <sugata.yuito.sw8@is.naist.jp>
2018-10-20 14:59:47 +0900

    Add trans

---
+++ hardware/IR/esp32_ir/esp32_ir.ino
@@ -0,0 +1,109 @@
+uint32_t width = 0;
+char tx_head[] = {0x40,0x04,0x07,0x20,0x00,0x00,0x00,0x60,0xCB};
+//char tx_head[] = {0xE7,0x30,0xD1,0x2E,0x16};
+//char tx_head[] = {0xE7,0x30,0xE9,0x16,0x16};
+char tx_data[] = {0x40,0x04,0x07,0x20,0x00,0x84,0x2C,0x01,0x22,0x00,0x00,0x60,0x06,0x00,0x00,0x01,0x00,0x60,0xD0,0x00};
+//char tx_data[] = {0};
+uint8_t i = 0, j = 0;
+int start_flag = 1;
+uint32_t start_t = 0;
+
+// use first channel of 16 channels (started from zero)
+#define LEDC_CHANNEL_0     0
+
+// use 13 bit precission for LEDC timer
+#define LEDC_TIMER_13_BIT  13
+
+// use 5000 Hz as a LEDC base frequency
+#define LEDC_BASE_FREQ     36700
+
+// fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
+#define LED_PIN 21
+
+void setup() 
+{
+  pinMode(A10, INPUT);
+//  pinMode(21, OUTPUT);
+//  digitalWrite(21, LOW);
+  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
+  Serial.begin(9600);
+
+  delay(1000);
+  Serial.println("Start");
+ 
+}
+
+void loop() 
+{
+  delay(100);
+  int head_len = sizeof(tx_head);
+//  Serial.println(head_len);
+  int data_len = sizeof(tx_data);
+
+  ledcWrite(LEDC_CHANNEL_0, 50);
+  delayMicroseconds(3500);
+
+  ledcWrite(LEDC_CHANNEL_0, 0);
+  delayMicroseconds(1500);
+
+  for(int i = 0; i < head_len; i++)
+  {
+    for(int j = 7; j >= 0; j--)
+    {
+      if(tx_head[i] & (1<<j))
+      {
+        ledcWrite(LEDC_CHANNEL_0, 50);
+        delayMicroseconds(430);
+        ledcWrite(LEDC_CHANNEL_0, 0);
+        delayMicroseconds(430 * 3);
+      }
+      else
+      {
+        ledcWrite(LEDC_CHANNEL_0, 50);
+        delayMicroseconds(430);
+        ledcWrite(LEDC_CHANNEL_0, 0);
+        delayMicroseconds(430 * 1);
+      }
+    }
+  }
+
+  ledcWrite(LEDC_CHANNEL_0, 50);
+  delayMicroseconds(430);
+
+  ledcWrite(LEDC_CHANNEL_0, 0);
+  delayMicroseconds(9960);
+  
+  ledcWrite(LEDC_CHANNEL_0, 50);
+  delayMicroseconds(3500);
+
+  ledcWrite(LEDC_CHANNEL_0, 0);
+  delayMicroseconds(1500);
+
+  for(int i = 0; i < data_len; i++)
+  {
+    for(int j = 7; j >= 0; j--)
+    {
+      if(tx_data[i] & (1<<j))
+      {
+        ledcWrite(LEDC_CHANNEL_0, 50);
+        delayMicroseconds(430);
+        ledcWrite(LEDC_CHANNEL_0, 0);
+        delayMicroseconds(430 * 3);
+      }
+      else
+      {
+        ledcWrite(LEDC_CHANNEL_0, 50);
+        delayMicroseconds(430);
+        ledcWrite(LEDC_CHANNEL_0, 0);
+        delayMicroseconds(430 * 1);
+      }
+    }
+  }
+  ledcWrite(LEDC_CHANNEL_0, 50);
+  delayMicroseconds(430);
+
+  ledcWrite(LEDC_CHANNEL_0, 0);
+  delayMicroseconds(30 * 430);
+
+}
