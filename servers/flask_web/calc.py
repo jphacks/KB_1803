e16c17510910690f44ce2d18acddcc47f0d79b4c
umetsuyoshinori <umetsu.yoshinori.uv4@is.naist.jp>
2018-11-16 22:08:06 +0900

    Fix requirements.txt

--- servers/flask_web/calc.py
+++ servers/flask_web/calc.py
@@ -1,14 +1,14 @@
-# import numpy as np
-# from sklearn import linear_model
-# from sklearn.externals import joblib
+import numpy as np
+from sklearn import linear_model
+from sklearn.externals import joblib
 
 def calc(dryness, temperature, humidity):
 
     # # テストデータ用意
-    # X_test = np.array([[dryness,humidity,temperature]])
+    X_test = np.array([[dryness,humidity,temperature]])
 
     # # 推定
-    # rest_of_time = prediction(X_test)
+    rest_of_time = prediction(X_test)
 
     # # 結果をリターン
     rest_of_time = 1
@@ -19,9 +19,9 @@ def prediction(X_test):
     # #X_testはnumpy.ndarrayでshapeは(1, 3)
 
     # # load the model
-    # clf = joblib.load('./model.pkl')
+    clf = joblib.load('./model.pkl')
 
     # # 作成したモデルから予測（学習済みモデル使用）
-    # y_test_pred = clf.predict(X_test)
+    y_test_pred = clf.predict(X_test)
 
     return int(y_test_pred[0]/60)
