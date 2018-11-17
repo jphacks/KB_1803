ad1fccb13e785bfdee76c45f2e5d71014bb09128
umetsuyoshinori <umetsu.yoshinori.uv4@is.naist.jp>
2018-11-17 13:39:13 +0900

    Fix model

--- servers/flask_web/calc.py
+++ servers/flask_web/calc.py
@@ -3,15 +3,13 @@ from sklearn import linear_model
 from sklearn.externals import joblib
 
 def prediction(X_test):
-    # #X_testはnumpy.ndarrayでshapeは(1, 3)
-
     # # load the model
     clf = joblib.load('./model.pkl')
 
     # # 作成したモデルから予測（学習済みモデル使用）
-    y_test_pred = clf.predict(X_test)
+    y_test_pred = clf.predict(X_test)[0]
     
-    return float(y_test_pred[0]/60 - 155.95807119) #モデルの100%のときの予測時間（秒）を引いて０にしてやる
+    return float(y_test_pred[0]/60 - 155.95807119469603) #モデルの100%のときの予測時間（秒）を引いて０にしてやる
 
 def calc(dryness):
 
@@ -24,6 +22,4 @@ def calc(dryness):
     # # 結果をリターン
     #rest_of_time = 1.0
     return rest_of_time
-
-
 
